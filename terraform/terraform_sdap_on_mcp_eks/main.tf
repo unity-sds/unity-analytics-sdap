978a91069df1a9d44a5d88e255dbbc8afe7233eb
Joseph Jacob <Joseph.Jacob@jpl.nasa.gov>
2024-03-07 13:41:15 -0800

    Added terraform to provision EKS cluster on MCP/AWS.

---
+++ terraform/terraform_sdap_on_mcp_eks/main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-west-2"
+}
+
+module "unity_eks" {
+  source = "./terraform-unity-eks_module"
+  tags = var.tags
+  deployment_name = var.deployment_name
+  nodegroups = var.nodegroups
+  aws_auth_roles = var.aws_auth_roles
+  project = var.project
+  venue = var.venue
+  installprefix = var.installprefix
+}
