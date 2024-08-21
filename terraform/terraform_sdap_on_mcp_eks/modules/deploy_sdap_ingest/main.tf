8ffb6c3c3f42f6322845f5471a774568c4738ea3
Joseph Jacob <Joseph.Jacob@jpl.nasa.gov>
2024-08-21 11:03:17 -0700

    Add terraform to deploy sdap data ingest components.

---
+++ terraform/terraform_sdap_on_mcp_eks/modules/deploy_sdap_ingest/main.tf
@@ -0,0 +1,59 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+    helm = {
+      source = "hashicorp/helm"
+    }
+    kubernetes = {
+      source = "hashicorp/kubernetes"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
+data "aws_eks_cluster_auth" "this" {
+  name = var.eks_cluster_name
+}
+
+provider "helm" {
+  kubernetes {
+    config_path = "~/.kube/config"
+    token = data.aws_eks_cluster_auth.this.token
+  }
+}
+
+provider "kubernetes" {
+  config_path = "~/.kube/config"
+  token = data.aws_eks_cluster_auth.this.token
+}
+
+data "kubernetes_secret" "rabbitmq_secret" {
+  metadata {
+    name = "rabbitmq"
+    namespace = var.namespace
+  }
+  binary_data = {
+    "rabbitmq-erlang-cookie" = ""
+    "rabbitmq-password" = ""
+  }
+}
+
+resource "helm_release" "sdap_ingest" {
+  upgrade_install = true
+  name = "sdap"
+  version = 1.0
+  chart = "../../../../helm"
+  namespace = var.namespace
+  values = [
+    "${file("../../../../k8s_deployment/values/values.yaml")}"
+  ]
+  set {
+    name = "rabbitmq.auth.erlangCookie"
+    value = base64decode(data.kubernetes_secret.rabbitmq_secret.binary_data.rabbitmq-erlang-cookie)
+  }
+}
